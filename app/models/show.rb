0acc316f1663fd6091485ca65452bd1440aa91dd
Flora Petersen <floramarina@comcast.net>
2020-08-17 23:40:19 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
 
   def actors_list
-    self.actors.collect do |actor|
+    self.characters.actors.collect do |actor|
       "#{actor.first_name} #{actor.last_name}"
     end
   end

b776b55455c32f488b2e2d2e232f448345e5ae38
Flora Petersen <floramarina@comcast.net>
2020-08-17 23:32:57 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,12 +2,6 @@ class Actor < ActiveRecord::Base
   has_many :characters
   has_many: :shows, through: :characters
 
-  def characters
-    self.characters.collect do |character|
-      "#{character.name}"
-    end
-  end
-
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
